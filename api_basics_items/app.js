ff396628dff6a2f2a697b5258eb7f6c4afa5d972
Katia Alvarez <A01781097@tec.mx>
2025-04-24 14:36:47 -0600

    UPDATE: app.js

--- api_basics_items/app.js
+++ api_basics_items/app.js
@@ -10,3 +10,22 @@ const app = express()
 app.use(express.json())
 
 app.use(express.static('./public'))
+
+app.get('/', (req, res)=>{
+    fs.readFile('./public/html/hello_server.html', 'utf8', 
+    (err, html) =>{
+        if(err)
+        {
+            res.status(500).send(`There was an error: ` + err)
+            return
+        }
+
+        console.log("Sending page...")
+        res.send(html)
+        console.log("Page sent!")
+        })
+})
+
+app.listen(port, ()=>{
+    console.log(`App listening on port: ${port}`)
+})
