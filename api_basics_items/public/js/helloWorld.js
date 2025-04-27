758d325f069a02be1131012a0112f1a9fc57ff80
Katia Alvarez <A01781097@tec.mx>
2025-04-27 02:21:06 -0600

    ADD: EndPoint to delete an item by its id

---
+++ api_basics_items/public/js/helloWorld.js
@@ -0,0 +1,18 @@
+async function deleteItemById(id){
+    const URL = `http://localhost:7500/items/${id}`;
+
+    const response = await fetch(URL, {
+        method: 'DELETE'
+    });
+
+    if (response.status === 200) {
+        const data = await response.json();
+        console.log(data.item);
+    } else if (response.status === 404) {
+        const error = await response.json();
+        console.log(error.message);
+    } 
+}
+
+deleteItemById(1);
+deleteItemById(6);
