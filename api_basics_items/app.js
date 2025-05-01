5705b28d1fbc3d36aa7087122eeb7f182eddf984
Katia Alvarez <A01781097@tec.mx>
2025-04-30 22:18:46 -0600

    ADD: basis of the API

---
+++ api_basics_items/app.js
@@ -0,0 +1,38 @@
+//Archivo - servidor que va a manejar todas las rutas (API endpoints)
+
+"use strict";
+
+import express from "express";
+import fs from 'fs'
+
+const port = 7600;
+
+const app = express()
+
+//Guardar items y usuarios
+const items = [];
+const users = []; 
+
+app.use(express.json())
+
+app.use(express.static('./public'))
+
+//Endpoint - cargar pagina
+app.get('/', (req, res)=>{
+    fs.readFile('./public/html/helloWorld.html', 'utf8', 
+    (err, html) =>{
+        if(err)
+        {
+            res.status(500).send(`There was an error: ` + err)
+            return
+        }
+
+        console.log("Sending page...")
+        res.send(html)
+        console.log("Page sent!")
+        })
+})
+
+app.listen(port, ()=>{
+    console.log(`App listening on port: ${port}`)
+})
